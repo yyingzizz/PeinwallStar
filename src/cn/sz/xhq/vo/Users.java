f6c4b8f29d6af8c204fe1ce041a68b2794e7e2ca
yyingzizz <474224126@qq.com>
2018-12-05 15:53:33 +0800

    分支提交测试

---
+++ src/cn/sz/xhq/vo/Users.java
@@ -0,0 +1,26 @@
+package cn.sz.xhq.vo;
+
+public class Users {
+
+	private Integer id;
+	private String username;
+	private String pwd;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+}
